19ce16d1b2467824abb67651ac8ad2620f86dd02
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 15:39:17 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        if((a1*n0 + a0 <= c*n0) && c>=a1) System.out.println(1);
+        else System.out.println(0);
+
+        br.close();
+    }
+}
