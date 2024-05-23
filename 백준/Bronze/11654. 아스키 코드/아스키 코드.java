a1ee1f233d043840c93aff3fe32a88970b1ac2b8
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 15:04:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+        public class Main{
+            public static void main(String[] args)  {
+                Scanner scanner = new Scanner(System.in);
+                String a = scanner.nextLine();
+                char as= a.charAt(0);
+                System.out.println((int) as);
+            }
+        }
