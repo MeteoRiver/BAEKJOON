435632547e05d2182f8bec26580d303c638b3230
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 13:10:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        System.out.print(a + b + c);
+    }
+}
