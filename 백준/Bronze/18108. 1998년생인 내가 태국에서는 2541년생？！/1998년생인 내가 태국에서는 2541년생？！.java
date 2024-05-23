c4aa3146456fd9e7b7a0590ff7134cb49721574c
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 13:08:14 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.print(year - 543);
+    }
+}
