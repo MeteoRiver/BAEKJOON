6cec33c5cc9fd49c3e15be31fc47ba08c5be5f11
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 16:29:45 +0900

    [Bronze II] Title: 단어의 개수, Time: 260 ms, Memory: 20020 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        bw.write(String.valueOf(st.countTokens()));
+
+        bw.flush();
+        bw.close();
+    }
+}
