35c95e3aa64af9f2abbf2abdaf084e8a8b04bb59
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 13:10:02 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a*(b%10));
+        System.out.println(a*(b/10%10));
+        System.out.println(a*(b/100));
+        System.out.println(a*b);
+    }
+}
