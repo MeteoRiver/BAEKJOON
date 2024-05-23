13d48d796050b4b65b8b435dab28dc43da237523
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 16:28:02 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 120 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long a=Long.parseLong(br.readLine());
+        System.out.println(a*a);
+        System.out.println("2");
+        }
+    }
