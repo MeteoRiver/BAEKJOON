42b61347bff26864fcf4bb5c444a8ac2dfd03aad
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-23 15:44:40 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1000 ms, Memory: 111272 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n=Integer.parseInt(br.readLine());
+        int[] arr=new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i]=Integer.parseInt(br.readLine());
+        }
+        Mergesort(arr);
+        for(int i = 0; i < n; i++){
+            bw.write((int)arr[i]+"\n");
+
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void Mergesort(int[] arr) {
+        int[] tmpArr = new int[arr.length];
+        mergeSort(arr,tmpArr,0,arr.length-1);
+    }
+    public static void mergeSort(int[] arr, int[] tmpArr, int left, int right) {
+        if (left < right) {
+            int m = left + (right - left) / 2;
+            mergeSort(arr, tmpArr, left, m);
+            mergeSort(arr, tmpArr, m + 1, right);
+            merge(arr, tmpArr, left, m, right);
+        }
+    }
+    public static void merge(int[] arr, int[] tmpArr, int left, int mid, int right) {
+        for (int i = left; i <= right; i++) {
+            tmpArr[i] = arr[i];
+        }
+        int part1 = left;
+        int part2 = mid + 1;
+        int index = left;
+        while (part1 <= mid && part2 <= right) {
+            if (tmpArr[part1] <= tmpArr[part2]) {
+                arr[index] = tmpArr[part1];
+                part1++;
+            } else {
+                arr[index] = tmpArr[part2];
+                part2++;
+            }
+            index++;
+        }
+        for (int i = 0; i <= mid - part1; i++) {
+            arr[index + i] = tmpArr[part1 + i];
+        }
+    }
+}
