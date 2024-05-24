02c2bddc89dd38e35c135b15a5c73ec2162dbfec
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-24 17:49:39 +0900

    [Bronze V] Title: 검증수, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            sum += a * a;
+        }
+        System.out.println(sum % 10);
+    }
+}
+
