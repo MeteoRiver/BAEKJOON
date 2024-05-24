b4cf71c725e04fc19438b10b2f9de252e534531b
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-24 17:00:24 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 17348 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+    import java.io.*;
+    import java.time.LocalDate;
+    import java.util.*;
+
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            LocalDate today = LocalDate.now();
+            System.out.println(today);
+        }
+    }
