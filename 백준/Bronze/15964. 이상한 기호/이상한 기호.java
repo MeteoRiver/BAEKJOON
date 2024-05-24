919f3eb9bf2e497654d8650113925f3c746699c1
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-05-24 17:47:01 +0900

    [Bronze V] Title: 이상한 기호, Time: 124 ms, Memory: 14172 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -1,15 +1,18 @@
-import java.io.*;
-import java.util.StringTokenizer;
+        import java.io.*;
+        import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        System.out.println((a+b)*(a-b));
+        public class Main {
+            public static void main(String[] args) throws IOException {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                long a = Integer.parseInt(st.nextToken());
+                long b = Integer.parseInt(st.nextToken());
+                long c = pro(a,b);
+                System.out.println(c);
 
-    }
-
-}
+            }
+            public static long pro(long a,long b){
+                return (a+b)*(a-b);
+            }
+        }
 
