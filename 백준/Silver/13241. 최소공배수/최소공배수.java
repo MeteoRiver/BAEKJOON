dbbab98af08761ba275fbfeb114e2fcd779b144c
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-06-03 15:48:28 +0900

    [Silver V] Title: 최소공배수, Time: 120 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+
+        System.out.println(a * b / gcd(a, b));
+
+
+    }
+
+    public static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
