427181f57303cba4716484c6b22d4a2641263deb
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2024-09-06 19:36:33 +0900

    [Bronze V] Title: A+B - 2, Time: 164 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            System.out.println(n+m);
+    }
+}
+
