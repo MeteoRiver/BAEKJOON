32af4c16f6ea0e7f7a113b2576a6d5f07bf5685c
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2025-01-20 19:01:14 +0900

    Create 703. Kth Largest Element in a Stream.java

---
+++ LeetCode/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
+    }
+}
