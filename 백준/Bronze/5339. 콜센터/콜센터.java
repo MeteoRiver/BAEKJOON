433e9bed9d6f50ffcfd2bd1785f163e6cef37498
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2025-02-12 18:04:42 +0900

    [Bronze V] Title: 콜센터, Time: 92 ms, Memory: 13832 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+            System.out.println("     /~\\\n" +
+                    "    ( oo|\n" +
+                    "    _\\=/_\n" +
+                    "   /  _  \\\n" +
+                    "  //|/.\\|\\\\\n" +
+                    " ||  \\ /  ||\n" +
+                    "============\n" +
+                    "|          |\n" +
+                    "|          |\n" +
+                    "|          |");
+    }
+}
