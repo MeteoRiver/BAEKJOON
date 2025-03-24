080f072eef6d24f7f4269572bfb39a9d3fea79c8
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2025-03-25 03:46:05 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((int) Math.sqrt(n));
+    }
+}
