429c58b3befaa0bb6b71ca9ef3fd02454e0656ad
EUNSEOK KANG <43646657+MeteoRiver@users.noreply.github.com>
2025-04-17 15:22:55 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int x = (int) Math.pow(2, n);
+        System.out.print(x);
+    }
+
+}
